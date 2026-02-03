6f15f2aa0f1dd8e7d83596550f94cf9581c0b33b
Marinela1193 <marpersuar2@alu.edu.gva.es>
2026-02-03 20:05:54 +0100

    creación DAO estudiantes

--- src/main/java/org/example/vtschool_mps_2526/models/dao/StudentsDAO.java
+++ src/main/java/org/example/vtschool_mps_2526/models/dao/StudentsDAO.java
@@ -1,4 +1,8 @@
 package org.example.vtschool_mps_2526.models.dao;
 
-public interface StudentsDAO {
+import org.example.vtschool_mps_2526.models.entities.StudentEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentsDAO extends CrudRepository<StudentEntity, Integer> {
+
 }
