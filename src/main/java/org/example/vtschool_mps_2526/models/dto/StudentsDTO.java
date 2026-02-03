6b8c8148b0262d9bcac00271685c7ec1b6b14ad9
Marinela1193 <marpersuar2@alu.edu.gva.es>
2026-02-03 20:06:02 +0100

    creación DTO estudiantes

--- src/main/java/org/example/vtschool_mps_2526/models/dto/StudentsDTO.java
+++ src/main/java/org/example/vtschool_mps_2526/models/dto/StudentsDTO.java
@@ -1,4 +1,60 @@
 package org.example.vtschool_mps_2526.models.dto;
 
 public class StudentsDTO {
+    private int  idcard;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+
+    public StudentsDTO() {
+    }
+
+    public StudentsDTO(int idcard, String firstName, String lastName, String phone, String email) {
+        this.idcard = idcard;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getIdcard() {
+        return idcard;
+    }
+
+    public void setIdcard(int idcard) {
+        this.idcard = idcard;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
